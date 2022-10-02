e683ab71e83b639e10f07cdf5b64cdfa22a20737
rimKhedher2022 <rimkhedher0@gmail.com>
2022-10-02 14:29:16 +0100

    02/10/2022

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,29 @@
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const CryptoJS = require("crypto-js")
 const router=require("express").Router();
+const User = require("../models/User")
+
+//update
+router.put("/:id",verifyTokenAndAuthorization, async(req,res)=>{
+   if(req.body.password){
+    req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+        ).toString();
+
+   }
+
+   try{
+    const updatedUser= await User.findByIdAndUpdate(req.params.id,{
+        $set:req.body,
+    },{new: true}
+    );
+
+    res.status(200).json(updatedUser);
+   }catch(err){
+    res.status(500).json(err);
+   }
+});
 
 
-
-module.exports=router
+module.exports=router;
